919baaa08f99e4b5adb1c3a0dcc356ccb43f0ae2
mixhael11 <mangelomasangkay@gmail.com>
2024-04-08 10:11:07 -0400

    gradeproject test

---
+++ app/src/main/java/newtester/AddTwoNumbers.java
@@ -0,0 +1,15 @@
+package newtester;
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        // Define two numbers to add
+        int num1 = 5;
+        int num2 = 7;
+        
+        // Add the numbers
+        int sum = num1 + num2;
+        
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
+
